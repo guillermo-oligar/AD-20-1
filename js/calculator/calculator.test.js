4b4ec792b5f501eb384d9a7671ae9e58027be8f0
Guillermo Olivares <guillermo.olivaresgar@gmail.com>
2025-09-28 22:24:01 -0600

    finished the tasks

--- js/calculator/calculator.test.js
+++ js/calculator/calculator.test.js
@@ -15,3 +15,23 @@ test('subtract 5 - 4 to equal 1', () => {
 test('subtract -5 - -10 to equal 5', () => {
   expect(calculator.subtract(-5, -10)).toBe(5);
 });
+
+test('divide 10 / 10 to equal 1', () => {
+  expect(calculator.divide(10, 10)).toBe(1);
+});
+
+test('divide -8 / 4 to equal 1', () => {
+  expect(calculator.divide(-8, 4)).toBe(-2);
+});
+
+test('divide 5 / 0 should throw error', () => {
+  expect(() => calculator.divide(5, 0)).toThrow("No se puede dividir por 0");
+});
+
+test('multiply 8 * 0 to equal 0', () => {
+  expect(calculator.multiply(8, 0)).toBe(0);
+});
+
+test('multiply 5 * -2 to equal -10', () => {
+  expect(calculator.multiply(5, -2)).toBe(-10);
+});
--- js/user/user-controller.test.js
+++ js/user/user-controller.test.js
@@ -16,3 +16,54 @@ test('remove user to userController', () => {
     expect(userController.users).not.toContain(user);
   });
 
+test("add user if not on the list", () => {
+  let userController = new UserController();
+  const newUser = new User (1234, "user not in the list", "testing@generation.org");
+  expect(userController.getUsers()).not.toContain(newUser);
+  userController.add(newUser);
+  expect(userController.getUsers()).toContain(newUser);
+});
+
+
+test("remove user who's not in the list", () => {
+  const userController = new UserController(); // lista vacía
+  const nonExistentUser = new User(1111, "Juan", "juan@generation.org");
+  userController.remove(nonExistentUser);
+  expect(userController.getUsers()).toHaveLength(0);
+});
+
+
+//para users
+
+test("findByEmail returns the correct user if email exists", () => {
+  const userController = new UserController();
+  const user = new User(1, "MK", "MK@generation.org");
+  userController.add(user);
+
+  expect(userController.findByEmail("MK@generation.org")).toBe(user);
+});
+
+test("findByEmail returns undefined if email does not exist", () => {
+  const userController = new UserController();
+  const user = new User(1, "Alice", "alice@generation.org");
+  userController.add(user);
+
+  expect(userController.findByEmail("bob@generation.org")).toBeUndefined();
+});
+
+
+test("findById returns the correct user if id exists", () => {
+  const userController = new UserController();
+  const user = new User(2, "Juan", "juan@generation.org");
+  userController.add(user);
+
+  expect(userController.findById(2)).toBe(user);
+});
+
+test("findById returns undefined if id does not exist", () => {
+  const userController = new UserController();
+  const user = new User(2, "Bob", "bob@generation.org");
+  userController.add(user);
+
+  expect(userController.findById(999)).toBeUndefined();
+});
